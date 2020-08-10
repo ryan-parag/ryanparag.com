c8d5aae1a48bd98e67211360fd8860c5df94282b
Ryan Parag <parag.ryan@gmail.com>
2020-08-10 01:10:47 -0400

    add cta & change button styles

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -2,22 +2,83 @@ import React from 'react'
 import styled from 'styled-components'
 import { designTokens } from '../Theme/designTokens'
 import { useForm, ValidationError } from '@statickit/react';
+import Link from 'next/link'
 
-const FormButton = styled.button`
-  font-family: inherit;
-  border: 1px solid var(--primary);
-  background: var(--primaryTransparent);
-  color: var(--primaryDark);
-  display: block;
+const ContactContainer = styled.div`
+  padding: ${designTokens.space[4]};
+  display: flex;
+  align-items: center;
+  border: 1px solid var(--grey200);
+  border-radius: ${designTokens.space[2]};
+`
+
+const ContactTitle = styled.h5`
+  font-size: ${designTokens.fontSizes[2]};
+  margin-top: 0;
+  margin-bottom: ${designTokens.space[2]};
+`
+
+const ContactContent = styled.div`
+  padding-left: ${designTokens.space[4]};
   width: 100%;
+  p {
+    font-size: ${designTokens.fontSizes[1]};
+    line-height: 150%;
+    color: var(--grey700);
+    margin-top: 0;
+  }
+  p:last-of-type {
+    margin-bottom: 0;
+  }
+`
+
+const LinkContainer = styled.div`
+  padding: ${designTokens.space[7]} 0 0;
+  display: flex;
+  justify-content: center;
+`
+
+const PostsLink = styled.span`
+  a {
+    padding: ${designTokens.space[2]} ${designTokens.space[3]};
+    display: inline-flex;
+    align-items:center;
+    font-weight: 700;
+    font-size: ${designTokens.fontSizes[1]};
+    border-radius: ${designTokens.space[2]};
+    border: 1px solid var(--grey200);
+    background: linear-gradient(to top, var(--grey100), var(--grey0));
+    box-shadow: 0px 1px 3px rgba(0,0,0,0.14);
+    transition: all 120ms ease-out 0s;
+    &:hover {
+      border-color: var(--grey300);
+      text-decoration: none;
+    }
+    &:focus {
+      box-shadow: 0px 0px 0px ${designTokens.space[1]} var(--primaryTransparent);
+    }
+  }
+`
+
+const Button = styled.button`
+  font-family: inherit;
+  border: 1px solid var(--primaryTransparent);
+  color: var(--primaryDark);
   padding: ${designTokens.space[3]};
-  border-radius: ${designTokens.space[1]};
-  margin-top: ${designTokens.space[4]};
+  margin-top: ${designTokens.space[5]};
   cursor: pointer;
   transition: all 120ms ease-out 0s;
+  border-radius: ${designTokens.space[2]};
+  background: var(--primaryTransparent);
+  box-shadow: 0px 1px 3px rgba(0,0,0,0.14);
   &:hover, &:focus {
-    border-color: var(--primaryTransparent);
+    border-color: var(--primary);
   }
+`
+
+const ButtonBlock = styled(Button)`
+  display: block;
+  width: 100%;
 `
 
 const EmptyStateIcon = styled.div`
@@ -39,16 +100,16 @@ const EmptyState = styled.div`
 `
 
 const ImgContainer = styled.div`
-  width: ${designTokens.space[8]};
-  height: ${designTokens.space[8]};
+  width: calc(${designTokens.space[7]} + ${designTokens.space[1]});
+  height: calc(${designTokens.space[7]} + ${designTokens.space[1]});
   display: inline-flex;
-  background: var(--grey200);
-  border: 1px solid var(--grey300);
-  color: var(--primary);
+  background: var(--primaryTransparent);
+  border: 1px solid var(--primaryTransparent);
   align-items: center;
   justify-content: center;
   padding: ${designTokens.space[1]};
-  border-radius: calc(${designTokens.space[4]} - ${designTokens.space[1]});
+  border-radius: 50%;
+  flex-shrink: 0;
 `
 
 const ImgProfile = styled.img`
@@ -79,43 +140,72 @@ export default function ContactForm() {
   }
 
   return(
-    <form onSubmit={handleSubmit}>
-      <ImgContainer>
-        <ImgProfile src="/static/thanks.png"/>
-      </ImgContainer>
-      <h3>Thanks for reading!</h3>
-      <p>Do you have feedback about this post or want to suggest an idea you'd like to hear about in a future post? Send me a message!</p>
-      <label htmlFor="message">
-        Message
-      </label>
-      <textarea
-        id="message"
-        name="message"
-        placeholder="What is your feedback or suggestion?"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-        style={{
-          color: 'var(--secondaryDark)',
-          fontSize: designTokens.fontSizes[1],
-          marginBottom: designTokens.space[3],
-          display: 'block'
-        }}
-      />
-      <label htmlFor="email">
-        Email Address (Optional)
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        placeholder="kimi.raikkonen@gmail.com"
-      />
-      <FormButton type="submit" disabled={state.submitting}>
-        Send Message
-      </FormButton>
-    </form>
+    <>
+      <ContactContainer>
+        <ImgContainer>
+          <ImgProfile src="/static/thanks.png"/>
+        </ImgContainer>
+        <ContactContent>
+          <ContactTitle>
+            Thanks for reading!
+          </ContactTitle>
+          <p>
+            I'm <a href="https://ryanparag.com">Ryan</a> and I'm a designer who codes - currently based in Tampa, FL. If you're looking for help or would like to chat, <a href="mailto:parag.ryan@gmail.com">reach out</a>!
+          </p>
+        </ContactContent>
+      </ContactContainer>
+      <LinkContainer>
+        <PostsLink>
+          <Link href="/notes/">
+            <a>
+              <img
+                src="/static/note.svg"
+                width="32"
+                style={{
+                  marginRight: designTokens.space[2],
+                  transform: 'rotate(10deg) translateX(-4px)'
+                }}
+              />
+              Read more notes
+            </a>
+          </Link>
+        </PostsLink>
+      </LinkContainer>
+      <hr/>
+      <form onSubmit={handleSubmit}>
+        <p>Do you have feedback about this post or want to suggest an idea you'd like to hear about in a future post? Send me a message!</p>
+        <label htmlFor="message">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="What is your feedback or suggestion?"
+        />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+          style={{
+            color: 'var(--secondaryDark)',
+            fontSize: designTokens.fontSizes[1],
+            marginBottom: designTokens.space[3],
+            display: 'block'
+          }}
+        />
+        <label htmlFor="email">
+          Email Address (Optional)
+        </label>
+        <input
+          id="email"
+          type="email" 
+          name="email"
+          placeholder="kimi.raikkonen@gmail.com"
+        />
+        <ButtonBlock type="submit" disabled={state.submitting}>
+          Send Message
+        </ButtonBlock>
+      </form>
+    </>
   )
 }
