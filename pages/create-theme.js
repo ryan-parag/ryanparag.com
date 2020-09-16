e9be2ddad8a2476ed66c15a2ed2395391ed91763
Ryan Parag <parag.ryan@gmail.com>
2020-09-16 01:10:01 -0400

    add selection to swatches

--- pages/create-theme.js
+++ pages/create-theme.js
@@ -25,7 +25,7 @@ const Card = styled.div`
 `
 const CardBody = styled.div`
   display: grid;
-  grid-template-columns: ${(props) => props.grid ? props.grid : '1fr 3fr'};
+  grid-template-columns: ${(props) => props.grid ? props.grid : '168px 3fr'};
   grid-column-gap: 0;
   @media screen and (max-width: ${designTokens.breakpoints[4]}) {
     grid-template-columns: 1fr;
@@ -53,12 +53,19 @@ const CardHeader = styled.div`
   justify-content: space-between;
 `
 
-const Swatch = styled.div`
+const Swatch = styled.button`
   width: ${designTokens.space[3]};
   height: ${designTokens.space[3]};
+  padding:0;
   margin: auto;
   border-radius: 50%;
-  border: 1px solid var(--borderColor);
+  border: ${(props) => props.active ? '2px solid var(--primary)' : '1px solid var(--borderColor)'};
+  box-shadow: ${(props) => props.active ? '0px 0px 0px 2px var(--primary)' : 'none'};
+  cursor: pointer;
+  transition: all 120ms ease-out 0s;
+  &:hover {
+    transform: scale(1.1);
+  }
 `
 
 const SwatchGrid = styled.div`
@@ -100,6 +107,8 @@ const CreateTheme = ({ title, description, ...props }) => {
   const [neutralLumEnd, setNeutralLumEnd] = useState(87)
   const [darkMode, setDarkMode] = useState(true)
   const [easing, setEasing] = useState('easeInQuad')
+  const [activeSwatch, setActiveSwatch] = useState(null)
+  const [activeSwatchName, setActiveSwatchName] = useState(null)
 
   const input = {
     specs: {
@@ -270,6 +279,11 @@ const CreateTheme = ({ title, description, ...props }) => {
     }
   }
 
+  const toggleActiveSwatch = (name, value) => {
+    setActiveSwatch(value)
+    setActiveSwatchName(name)
+  }
+
   const spacings = [
     { name: 'Quad - EaseIn', value: 'easeInQuad'},
     { name: 'Quad - EaseOut', value: 'easeOutQuad'},
@@ -324,27 +338,41 @@ const CreateTheme = ({ title, description, ...props }) => {
               </CardRow>
               <CardRow>
                 <SwatchGrid>
-                  <Swatch style={{ background: customTheme.grey900 }}/>
-                  <Swatch style={{ background: customTheme.grey800 }}/>
-                  <Swatch style={{ background: customTheme.grey700 }}/>
-                  <Swatch style={{ background: customTheme.grey600 }}/>
-                  <Swatch style={{ background: customTheme.grey500 }}/>
-                  <Swatch style={{ background: customTheme.grey400 }}/>
-                  <Swatch style={{ background: customTheme.grey300 }}/>
-                  <Swatch style={{ background: customTheme.grey200 }}/>
-                  <Swatch style={{ background: customTheme.grey100 }}/>
-                  <Swatch style={{ background: customTheme.grey0 }}/>
-                  <Swatch style={{ background: customTheme.primary }}/>
-                  <Swatch style={{ background: customTheme.primaryTransparent }}/>
-                  <Swatch style={{ background: customTheme.primaryDark }}/>
-                  <Swatch style={{ background: customTheme.secondary }}/>
-                  <Swatch style={{ background: customTheme.secondaryTransparent }}/>
-                  <Swatch style={{ background: customTheme.secondaryDark }}/>
-                  <Swatch style={{ background: customTheme.tertiary }}/>
-                  <Swatch style={{ background: customTheme.tertiaryTransparent }}/>
-                  <Swatch style={{ background: customTheme.tertiaryDark }}/>
-                  <Swatch style={{ background: customTheme.transparent }}/>
+                  <Swatch active={activeSwatch === customTheme.grey900 ? true : false} onClick={() => toggleActiveSwatch("Grey 900", customTheme.grey900)} title="Grey 900" style={{ background: customTheme.grey900 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey800 ? true : false} onClick={() => toggleActiveSwatch("Grey 800", customTheme.grey800)} title="Grey 900" style={{ background: customTheme.grey800 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey700 ? true : false} onClick={() => toggleActiveSwatch("Grey 700", customTheme.grey700)} title="Grey 800" style={{ background: customTheme.grey700 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey600 ? true : false} onClick={() => toggleActiveSwatch("Grey 600", customTheme.grey600)} title="Grey 600" style={{ background: customTheme.grey600 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey500 ? true : false} onClick={() => toggleActiveSwatch("Grey 500", customTheme.grey500)} title="Grey 500" style={{ background: customTheme.grey500 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey400 ? true : false} onClick={() => toggleActiveSwatch("Grey 400", customTheme.grey400)} title="Grey 400" style={{ background: customTheme.grey400 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey300 ? true : false} onClick={() => toggleActiveSwatch("Grey 300", customTheme.grey300)} title="Grey 300" style={{ background: customTheme.grey300 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey200 ? true : false} onClick={() => toggleActiveSwatch("Grey 200", customTheme.grey200)} title="Grey 200" style={{ background: customTheme.grey200 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey100 ? true : false} onClick={() => toggleActiveSwatch("Grey 100", customTheme.grey100)} title="Grey 100" style={{ background: customTheme.grey100 }}/>
+                  <Swatch active={activeSwatch === customTheme.grey0 ? true : false} onClick={() => toggleActiveSwatch("Grey 0", customTheme.grey0)} title="Grey 0" style={{ background: customTheme.grey0 }}/>
+                  <Swatch active={activeSwatch === customTheme.primary ? true : false} onClick={() => toggleActiveSwatch("Primary", customTheme.primary)} title="Primary" style={{ background: customTheme.primary }}/>
+                  <Swatch active={activeSwatch === customTheme.primaryTransparent ? true : false} onClick={() => toggleActiveSwatch("Primary Transparent", customTheme.primaryTransparent)} title="Primary Transparent" style={{ background: customTheme.primaryTransparent }}/>
+                  <Swatch active={activeSwatch === customTheme.primaryDark ? true : false} onClick={() => toggleActiveSwatch("Primary Dark", customTheme.primaryDark)} title="Primary Dark" style={{ background: customTheme.primaryDark }}/>
+                  <Swatch active={activeSwatch === customTheme.secondary ? true : false} onClick={() => toggleActiveSwatch("Secondary", customTheme.secondary)} title="Secondary" style={{ background: customTheme.secondary }}/>
+                  <Swatch active={activeSwatch === customTheme.secondaryTransparent ? true : false} onClick={() => toggleActiveSwatch("Secondary Transparent", customTheme.secondaryTransparent)} title="Secondary Transparent" style={{ background: customTheme.secondaryTransparent }}/>
+                  <Swatch active={activeSwatch === customTheme.secondaryDark ? true : false} onClick={() => toggleActiveSwatch("Secondary Dark", customTheme.secondaryDark)} title="Secondary Dark" style={{ background: customTheme.secondaryDark }}/>
+                  <Swatch active={activeSwatch === customTheme.tertiary ? true : false} onClick={() => toggleActiveSwatch("Tertiary", customTheme.tertiary)} title="Tertiary" style={{ background: customTheme.tertiary }}/>
+                  <Swatch active={activeSwatch === customTheme.tertiaryTransparent ? true : false} onClick={() => toggleActiveSwatch("Tertiary Transparent", customTheme.tertiaryTransparent)} title="Tertiary Transparent" style={{ background: customTheme.tertiaryTransparent }}/>
+                  <Swatch active={activeSwatch === customTheme.tertiaryDark ? true : false} onClick={() => toggleActiveSwatch("Tertiary Dark", customTheme.tertiaryDark)} title="Tertiary Dark" style={{ background: customTheme.tertiaryDark }}/>
+                  <Swatch active={activeSwatch === customTheme.transparent ? true : false} onClick={() => toggleActiveSwatch("Transparent", customTheme.transparent)} title="Transparent" style={{ background: customTheme.transparent }}/>
                 </SwatchGrid>
+                {
+                  activeSwatch !== null ?
+                  (
+                    <div style={{ padding: `${designTokens.space[2]} 0` }}>
+                      <small>
+                        <strong>{activeSwatchName}</strong>
+                        <br/>
+                        {activeSwatch}
+                      </small>
+                    </div>
+                  )
+                  :
+                    null
+                }
               </CardRow>
             </CardColumn>
             <CardColumn>
@@ -452,7 +480,7 @@ const CreateTheme = ({ title, description, ...props }) => {
               <CardBody grid={'repeat(2, 1fr)'}>
                 <CardColumn>
                   <CardRow bottomBorder>
-                    <SectionTitle spacing="true">Primary {customTheme.primary}</SectionTitle>
+                    <SectionTitle spacing="true">Primary</SectionTitle>
                     <ColorPicker
                       color={customTheme.primary}
                       changeColor={changePrimary}
