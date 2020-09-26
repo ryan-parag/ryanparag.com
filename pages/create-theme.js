3f02c809cae567f0566903e37485b67751549f00
Ryan Parag <parag.ryan@gmail.com>
2020-09-26 13:59:25 -0400

    replace og information with page sepcific info

--- pages/create-theme.js
+++ pages/create-theme.js
@@ -9,7 +9,7 @@ const CreateTheme = ({ title, description, ...props }) => {
 
   return (
     <>
-      <Layout pageTitle={`${title} | Create a Theme`} description={description} ogImage="/notes-social-media.png">
+      <Layout pageTitle={`${title} | Create a Theme`} description={description} ogImage="../static/designing-for-personalization.png">
         <h2>Create a Theme 🎨</h2>
         <p>I like to believe design is a fluid skill and that <strong>everyone is a bit of a designer</strong> 👍 - we all have the ability to feel certain ways about things that are designed.</p>
         <p>Many of us are also naturally curious and enjoy tinkering with things - so why not <strong>feed that curiousity</strong> and play around with creating a new theme for this website! <strong>Have fun!</strong></p>
