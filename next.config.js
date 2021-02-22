2263cb029e2067ceb1a221cd03fbc193ad41ce44
Ryan Parag <parag.ryan@gmail.com>
2021-02-21 22:19:37 -0500

    change config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ module.exports = {
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
     SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN
   },
-  target: 'serverless',
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
