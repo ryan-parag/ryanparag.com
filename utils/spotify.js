016facde4d28911d1890f37bc041c7799c864a34
Ryan Parag <parag.ryan@gmail.com>
2021-02-21 22:12:44 -0500

    test api

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,8 +1,12 @@
 import querystring from 'querystring';
 
-const client_id = process.env.SPOTIFY_CLIENT_ID
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET
-const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
+//const client_id = process.env.SPOTIFY_CLIENT_ID
+//const client_secret = process.env.SPOTIFY_CLIENT_SECRET
+//const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
+
+const client_id = "fb47075d3d544037a69493e74b4424f5"
+const client_secret = "f2f6fc974e574d3087f977acef6a1335"
+const refresh_token = "AQBLK4Jg4weblcdk4InW6MAuBWdB_5hdNvV1XI_QegqP7kedEGeXXTPXF9BVsAp1Z_LgItNqklXlVvDw0mi22LUs_GDIssBXKp756FE1HwF2eVtJfS7aaQ7cfCNcY96qOR4"
 
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const LAST_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`;
