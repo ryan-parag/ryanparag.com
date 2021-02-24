712112122fd9d086f2544373c1517147bc2b40a1
Ryan Parag <parag.ryan@gmail.com>
2021-02-23 21:53:44 -0500

    revert config

--- next.config.js
+++ next.config.js
@@ -7,5 +7,12 @@ module.exports = {
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
     SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN
   },
-  target: 'serverless'
+  target: 'serverless',
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
+    return config
+  },
 }
