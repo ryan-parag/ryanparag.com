b3c623ca1e1e06ed0ce5989956f7ed734520ff5e
Ryan Parag <parag.ryan@gmail.com>
2021-02-28 22:38:04 -0500

    aria-labels on icon buttons

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,6 +8,7 @@ import { IconButton, IconButtonPrimary } from '@components/Button'
 import { motion } from 'framer-motion'
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
+import { X } from 'react-feather'
 
 const HeaderContainer = styled.div`
   position: relative;
@@ -213,6 +214,7 @@ const ThemeBtn = ({handleClick, state}) => {
   return(
     <IconButton
       onClick={handleClick}
+      aria-label={state ? 'Close Themes' : 'Open Themes'}
     >
       {
         state ?
@@ -370,7 +372,7 @@ export default function Header({ toggleTheme, theme }) {
           <CloseButton
            onClick={toggle}
           >
-            &times;
+            <X size={'16'}/>
           </CloseButton>
         </div>
         <ThemePickerBody>
@@ -391,6 +393,7 @@ export default function Header({ toggleTheme, theme }) {
                   style={{
                     transform: `translateY(${designTokens.space[3]})`
                   }}
+                  aria-label={'Create Theme'}
                 >
                   <svg width="16" height="16" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12.7909 24.5781C13.6792 24.5781 14.3984 23.8449 14.3984 23.0129V14.1856H23C23.8602 14.1856 24.5935 13.4665 24.5935 12.5781C24.5935 11.7039 23.8602 10.9706 23 10.9706H14.3984V2.14334C14.3984 1.29728 13.6792 0.578125 12.7909 0.578125C11.9166 0.578125 11.1833 1.29728 11.1833 2.14334V10.9706H2.5817C1.74974 10.9706 0.988281 11.7039 0.988281 12.5781C0.988281 13.4665 1.74974 14.1856 2.5817 14.1856H11.1833V23.0129C11.1833 23.8449 11.9166 24.5781 12.7909 24.5781Z" fill="currentColor"/>
@@ -407,13 +410,13 @@ export default function Header({ toggleTheme, theme }) {
             <div style={{ display: 'inline-flex', alignItems: 'center'}}>
               {
                 isExpanded ? (
-                  <IconButton onClick={closeMobile}>
+                  <IconButton onClick={closeMobile} aria-label={'Close'}>
                     <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 512 512'><path d='M289.94,256l95-95A24,24,0,0,0,351,127l-95,95-95-95A24,24,0,0,0,127,161l95,95-95,95A24,24,0,1,0,161,385l95-95,95,95A24,24,0,0,0,385,351Z'/></svg>
                   </IconButton>
                 )
                 :
                 (
-                  <IconButton onClick={() => setExpanded(true)}>
+                  <IconButton onClick={() => setExpanded(true)} aria-label={'Open'}>
                     <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 512 512'><line x1='88' y1='152' x2='424' y2='152' fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48px"/><line x1='88' y1='256' x2='424' y2='256' fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48px"/><line x1='88' y1='360' x2='424' y2='360' fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48px"/></svg>
                   </IconButton>
                 )
