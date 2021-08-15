2a51f3348375d7369eddfe5b2c81ef7ee9d4f5d3
Ryan Parag <parag.ryan@gmail.com>
2021-08-14 20:37:01 -0400

    add scroll button

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -5,7 +5,7 @@ import Layout, { Wrapper } from '@components/Layout/'
 import Title from '@components/Title'
 import { PortfolioList, Form } from '@components/Portfolios'
 import styled from 'styled-components'
-import { SmallButton } from '@components/Button'
+import { SmallButton, Button } from '@components/Button'
 import { designTokens } from '@components/Theme/designTokens'
 
 const SearchFilter = styled.div`
@@ -21,9 +21,28 @@ const SearchFilter = styled.div`
   }
 `
 
+const ScrolledButton = styled(Button)`
+  position: fixed;
+  bottom: ${designTokens.space[3]};
+  right: ${designTokens.space[3]};
+  @media screen and (max-width: ${designTokens.breakpoints[4]}) {
+    display: none;
+  }
+`
+
 const Page = ({ token, title, description, ...props }) => {
 
   const [filterString, setFilterString] = useState('')
+
+  const scrollToTop = () => {
+    if(process.browser) {
+      window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth'
+      });
+    }
+  }
 
   const { data } = useSWR('/api/portfolios/list', fetcher);
 
@@ -63,6 +82,11 @@ const Page = ({ token, title, description, ...props }) => {
             filterString={filterString}
           />
         </Wrapper>
+        <ScrolledButton
+          onClick={() => scrollToTop()}
+        >
+          Scroll to Top
+        </ScrolledButton>
       </Layout>
     </>
   )
